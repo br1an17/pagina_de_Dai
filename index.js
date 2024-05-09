74d188646e8d571e1bfbc0421c19974398f54873
br1an17 <106201381+br1an17@users.noreply.github.com>
2024-05-09 15:17:35 -0300

    agregamos carpeta src para organisar codigo

---
+++ index.js
@@ -0,0 +1,15 @@
+// const express = require("express")
+
+// const app =express()
+
+
+
+// app.get("/",(req,res)=>{
+// res.sendFile("/src/home.html",{
+//     root:__dirname
+// })
+
+// })
+
+// app.listen(3000)
+// console.log(`puerto${3000}`)
