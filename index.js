82e4a9948add7da2439653a96b61918309d2d140
br1an17 <brianzairpaez@gmail.com>
2025-07-25 18:31:01 -0300

    arreglamos el regreso al index

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
 let usuario = prompt("ingresa tu nombre")
 
-alert("Bienvenido " + usuario + " a la pagina de Dai")
+if(usuario != null){alert("Bienvenido " + usuario + " a la pagina de Dai")
+}else{alert("Bienvenido a la pagina de Dai")}
